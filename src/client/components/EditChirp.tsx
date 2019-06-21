4abc9589ec8fcd4e16f2c523fdd667c62e65e21f
solgamd <solgamd@gmail.com>
2019-06-21 18:57:37 +0200

    Resolved errors in EditChirp. App still crashing.

--- src/client/components/EditChirp.tsx
+++ src/client/components/EditChirp.tsx
@@ -1,21 +1,40 @@
 import * as React from 'react';
 import ChirpCard from "./ChirpCard";
 
-
-export interface IEditProps {
+export interface IEditProps {}
+export interface IEditState {
     chirp: {
         id: number,
         user: string,
         text: string
+    }[]
+};
+
+class EditChirp extends React.Component<IEditProps, IEditState> {
+    
+    constructor(props: IEditProps) {
+        super(props)
+        this.state = { chirp: [] }
     }
-}
- 
-const EditChirp: React.SFC<IEditProps> = (props) => {
-    return (
+
+    async componentWillMount() {
+        try {
+            let res = await fetch("/api/chirps/:id");
+            let chirp = await res.json();
+            this.setState({ chirp });
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    render() {
+        return (
         <section className="row">
-            <ChirpCard chirp={chirp.id} />
+            {this.state.chirp.map(aChirp => <ChirpCard key={aChirp.id} chirp={aChirp} />)}
         </section>
       );
+    }
+    
 }
  
 export default EditChirp;
